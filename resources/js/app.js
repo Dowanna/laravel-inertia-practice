33d49a2930afb1310891878785a473953b18bf7c
shunya matsubara <shun.fzs1000@gmail.com>
2022-07-06 11:36:01 +0900

    chakra-uiを追加してみる
    refs: #3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,20 @@
 import './bootstrap';
 
 import React from 'react'
-import { render } from 'react-dom'
+// import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import { InertiaApp } from '@inertiajs/inertia-react';
+import { ChakraProvider } from '@chakra-ui/react'
 
-const app = document.getElementById('app');
-
-render(
+const container = document.getElementById('app');
+const root = createRoot(container)
+root.render(
+	<ChakraProvider>
 	<InertiaApp
-	  initialPage={JSON.parse(app.dataset.page)}
-	  resolveComponent={name =>
+		initialPage={JSON.parse(app.dataset.page)}
+		resolveComponent={name =>
 		import(`./Pages/${name}`).then(module => module.default)
-	  }
-	/>,
-	app
-  );
+		}
+	/>
+	</ChakraProvider>,
+)
